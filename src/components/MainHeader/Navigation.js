04b081216d30039ceed50cbd4e977e59530c282d
ubisage <obaiduzair822@gmail.com>
2023-01-13 15:53:53 +0530

    passed data without passing via props

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -33,16 +33,53 @@
 
 
 // <----------Context--------> 
-import React from 'react';
+// import React from 'react';
+// import AuthContext from '../../store/auth-context';
+
+// import classes from './Navigation.module.css';
+
+// const Navigation = (props) => {
+//   return (
+//     <AuthContext.Consumer>
+//       {(ctx)=>{
+//         return <nav className={classes.nav}>
+//         <ul>
+//           {ctx.isLoggedIn && (
+//             <li>
+//               <a href="/">Users</a>
+//             </li>
+//           )}
+//           {ctx.isLoggedIn && (
+//             <li>
+//               <a href="/">Admin</a>
+//             </li>
+//           )}
+//           {ctx.isLoggedIn && (
+//             <li>
+//               <button onClick={props.onLogout}>Logout</button>
+//             </li>
+//           )}
+//         </ul>
+//       </nav>
+//       }}
+    
+//     </AuthContext.Consumer>
+//   );
+// };
+
+// export default Navigation;
+
+
+
+// <---------- USE Context HOOK--------> 
+import React, { useContext} from 'react';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
-  return (
-    <AuthContext.Consumer>
-      {(ctx)=>{
-        return <nav className={classes.nav}>
+  const ctx = useContext(AuthContext)
+  return ( <nav className={classes.nav}>
         <ul>
           {ctx.isLoggedIn && (
             <li>
@@ -61,10 +98,11 @@ const Navigation = (props) => {
           )}
         </ul>
       </nav>
-      }}
+      
     
-    </AuthContext.Consumer>
   );
 };
 
 export default Navigation;
+
+
